9d5b10f5869c72ba999ea5290a2834688f170e4e
fcakiroglu16 <f-cakiroglu@outlook.com>
2025-12-17 22:05:08 +0300

    Add RabbitMQExample project with user and queue logic
    Added new RabbitMQExample .NET 10 console app to the solution. Implemented User and UserService classes with queue message delivery logic and retry handling. Updated Program.cs with a basic entry point and referenced the new project in the solution file. Replaced old UserService.cs binary with new source code.

---
+++ RabbitMQExample/Producer/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQExample.Producer;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Email { get; set; }
+
+    public required string PhoneNumber { get; set; }
+}
